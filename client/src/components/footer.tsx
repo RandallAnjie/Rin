2c5291eaaeaf92bf5b17479277e7dcb84bffa2ac
Randall <randall@randallanjie.com>
2025-07-31 13:18:05 +0800

    fix: 更新页脚版权名

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -59,7 +59,7 @@ function Footer() {
                             setDoubleClickTimes(doubleClickTimes + 1)
                         }
                     }}>
-                        © {new Date().getFullYear()} Powered by <a className='hover:underline' href="https://github.com/openRin/Rin" target="_blank">Rin</a>
+                        © {new Date().getFullYear()} Powered by <a className='hover:underline' href="https://randallanjie.com" target="_blank">Randall</a>
                     </span>
                     {config.get<boolean>('rss') && <>
                         <Spliter />
